68b3a18e3d581a5e24461d5c342e692e765eef6b
Alessandro Candido <candido.ale@gmail.com>
2020-10-29 22:09:17 +0100

    Reorganize structure as a package, preliminart attempt

---
+++ setup.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+# write version on the fly - inspired by numpy
+MAJOR = 0
+MINOR = 1
+MICRO = 4
+ISRELEASED = False
+SHORT_VERSION = "%d.%d" % (MAJOR, MINOR)
+VERSION = "%d.%d.%d" % (MAJOR, MINOR, MICRO)
+
+
+def write_version_py(filename="src/libmake/version.py"):
+    cnt = """
+# THIS FILE IS GENERATED FROM SETUP.PY
+major = %(major)d
+short_version = '%(short_version)s'
+version = '%(version)s'
+full_version = '%(full_version)s'
+is_released = %(isreleased)s
+"""
+    FULLVERSION = VERSION
+    if not ISRELEASED:
+        FULLVERSION += "-develop"
+
+    a = open(filename, "w")
+    try:
+        a.write(
+            cnt
+            % {
+                "major": MAJOR,
+                "short_version": SHORT_VERSION,
+                "version": VERSION,
+                "full_version": FULLVERSION,
+                "isreleased": str(ISRELEASED),
+            }
+        )
+    finally:
+        a.close()
+
+
+def setup_package():
+    # write version
+    write_version_py()
+    # paste Readme
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+    # do it
+    setup(
+        name="slides-reveal",
+        version=VERSION,
+        description="Make slides with reveal.js",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author="A.Candido",
+        author_email="candido.ale@gmail.com",
+        url="https://github.com/AleCandido/slides-reveal",
+        package_dir={"": "src"},
+        packages=find_packages("src"),
+        classifiers=[
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+        ],
+        install_requires=[
+            # make management
+            "click",
+            "httpwatcher",
+            # configs
+            "toml",
+            # templates
+            "jinja2",
+            # structures
+            "python-frontmatter",
+        ],
+        setup_requires=["wheel"],
+        python_requires=">=3.7",
+    )
+
+
+if __name__ == "__main__":
+    setup_package()
