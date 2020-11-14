d9b578872a51217bc8e92549f4830a811dc3a363
Alessandro Candido <candido.ale@gmail.com>
2020-11-14 12:02:14 +0100

    Bump version to 0.1.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 # write version on the fly - inspired by numpy
 MAJOR = 0
 MINOR = 1
-MICRO = 3
+MICRO = 4
 
 # Further release management
 # --------------------------
